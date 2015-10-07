a30016e68a6e45beefe95c7f515236e2c71f4ed5
Michael Sotkin <michael.sotkin@gmail.com>
2015-10-07 15:05:00 -0700

    Sorcery authentication complete

---
+++ app/views/authors/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @author, :id, :username, :e-mail, :password, :password_confirmation, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ resources :articles do
 	resources :comments
 end
 resources :tags
+resources :authors
+resources :author_sessions, only: [:new, :create, :destroy]
+
+get 'login' => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
 end
